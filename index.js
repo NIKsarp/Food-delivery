3891d4a3e4cee6f1dd190a73536eee86852206d6
Prashant Rastogi <publicidforpeople>
2025-07-13 19:20:31 +0530

    add files

---
+++ index.js
@@ -0,0 +1,8 @@
+// --------------------------------------------------
+// Copyright Year Start
+{
+let date = new Date().getFullYear();
+document.getElementById("currentYear").innerHTML = date;
+}
+// Copyright Year End
+// --------------------------------------------------
